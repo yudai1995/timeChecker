77ed5c84644feb04a0850718fc40f906d35e3f7a
yamamoto <y.yamamoto991103@gmail.com>
2025-03-24 03:25:16 +0900

    リポジトリリンク追加

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@ import TimeCheck from './components/TimeCheck.vue'
   <div class="content">
     <header>
       <h1>時間範囲チェック</h1>
+      <div class="codeLink">
+        <a href="https://github.com/yudai1995/timeChecker" target="_blank" rel="noopener"
+          >Githubリポジトリ</a
+        >
+      </div>
     </header>
     <main>
       <TimeCheck />
@@ -22,6 +27,23 @@ import TimeCheck from './components/TimeCheck.vue'
 <style scoped>
 header {
   line-height: 1.5;
+  text-align: center;
+
+  .codeLink a {
+    display: inline-block;
+    margin: 10px 0;
+    padding: 10px 10px;
+    font-size: 14px;
+    color: var(--vt-c-divider-dark-1);
+    border: 1px solid #ddd;
+    border-radius: 6px;
+    background-color: #ffffff;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #f5f5f5;
+    }
+  }
 }
 
 footer {
@@ -49,6 +71,12 @@ footer {
     header,
     main {
       width: 100%;
+    }
+
+    header {
+      .codeLink a {
+        margin: 10px;
+      }
     }
   }
 }
